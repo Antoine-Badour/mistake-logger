d34b8a075db0ad056251ac6a5fc43a28e7d4dcc5
Antoine-Badour <antoine.badour@gmail.com>
2026-07-05 13:24:30 +0300

    plizzz

--- api/config.js
+++ api/config.js
@@ -1,7 +1,10 @@
+const DEFAULT_SUPABASE_URL = 'https://prjkktsdmvbmdqduvqzz.supabase.co';
+const DEFAULT_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByamtrdHNkbXZibWRxZHV2cXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyNDAzNjAsImV4cCI6MjA5ODgxNjM2MH0.Fxa2UoeUhQMDpt_HqeFLeBB-BLqXuTAOeqlYupsx-0M';
+
 module.exports = (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.status(200).json({
-    supabaseUrl: process.env.SUPABASE_URL || '',
-    supabaseAnonKey: process.env.SUPABASE_ANON_KEY || ''
+    supabaseUrl: process.env.SUPABASE_URL || DEFAULT_SUPABASE_URL,
+    supabaseAnonKey: process.env.SUPABASE_ANON_KEY || DEFAULT_SUPABASE_ANON_KEY
   });
 };
